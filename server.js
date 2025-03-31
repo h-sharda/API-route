80df18642e4e275d57d4899bbe63fc876ba9f1e7
Vector <harshitsharda0@gmail.com>
2025-03-31 22:59:29 +0530

    updates

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.listen(PORT, () => {
 });
 
 function generateRandomString(length) {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let result = '';
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * characters.length);
